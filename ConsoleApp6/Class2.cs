76acb33b626c0e993e6f79db59360913c7c67c37
pvinyareddy <vinyareddyp@gmail.com>
2023-08-17 16:26:34 -0400

    ConsoleApp6

---
+++ ConsoleApp6/Class2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Class2
+    {
+       
+
+            static void Main()
+            {
+                Console.WriteLine("Enter a string:");
+                string input = Console.ReadLine();
+                string result = new string(input.Distinct().ToArray());
+                Console.WriteLine("String with duplicates removed: " + result);
+                Console.ReadLine();
+            }
+    }
+
+}
